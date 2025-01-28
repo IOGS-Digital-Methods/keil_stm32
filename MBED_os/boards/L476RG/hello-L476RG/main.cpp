c7475c69574c2bf81131fb09e9ffc9a047257401
Julien Villemejane <julien.villemejane@institutoptique.fr>
2025-01-28 21:44:18 +0100

    Create a Hello program for L476RG - baremetal style - blinky

--- MBED_os/boards/L476RG/hello-L476RG/main.cpp
+++ MBED_os/boards/L476RG/hello-L476RG/main.cpp
@@ -1,12 +1,32 @@
-#include "mbed.h" 
+/*  
+ *  Structure of a main file for embedded project @ LEnsE
+ *      Robotics Project / Ultrasound distance sensor
+ *
+ *  This code allows to get the distance from an ultrasound sensor. 
+ *****************************************************************
+ *  Pinout :
+ *      PB_5 - Output - US trig input 
+ *      PB_3 - Input - US echo output 
+ *****************************************************************
+ *  Tested with Nucleo XnnnMM board / Mbed OS 6
+ *****************************************************************
+ *  Author : J. VILLEMEJANE / LEnsE - Creation 2025/01/18
+ *****************************************************************
+ *  LEnsE / https://lense.institutoptique.fr/
+ *      Based on Mbed OS 6 example : mbed-os-example-blinky-baremetal
+ */
 
-DigitalOut led(LED1); 
+#include "mbed.h"
 
+DigitalOut  led1(LED1);
 
-int main(){ 
+int main()
+{
+		printf("Hello L476RG - LEnsE !!\r\n");
 
-	while (1) { 
-		led = !led; 
-		thread_sleep_for(50); 
-	} 
+    while(1)
+    {
+				led1 = !led1;
+        thread_sleep_for(400);
+    }
 }
